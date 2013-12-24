3e1db40de0550d50b74c4f273e51ff1029613111
Doc Walker <doc.walker@jameshardie.com>
2013-12-24 13:21:46 -0600

    Fix #15 update to match coding style of jhx/gem-*

--- spec/test_app/app/assets/javascripts/application.js
+++ spec/test_app/app/assets/javascripts/application.js
@@ -1,1 +1,3 @@
+// provides HTML5 placeholder behavior for old browsers
+// from gem 'enableplaceholder-jquery-rails':
 //= require enableplaceholder-jquery-rails
--- spec/test_app/config/initializers/secret_token.rb
+++ spec/test_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TestApp::Application.config.secret_token = '65a9756396061e1df1abad006962e69f21906a1edaf1b2b50dae89b3a53459bad80c10d9b14e5ab7767583a0aeaf40ed88c52d86d06a809e640268ead49d5a1e'
+TestApp::Application.config.secret_token = '1071c3d9add89ef5bc73856c815112ffd44c025d9eea5c80bf29cde4fb0e022e44b81498ff27535864e740d354ebe4913049e2d989228746cdb046e3f9f34a68'
