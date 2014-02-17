4762efbd127153a8480a0ab3723a1981fe41b9ec
Doc Walker <doc.walker@jameshardie.com>
2014-02-16 20:21:45 -0600

    Fix rubocop MethodCallParentheses offense

--- spec/enableplaceholder-jquery-rails_spec.rb
+++ spec/enableplaceholder-jquery-rails_spec.rb
@@ -5,7 +5,7 @@ describe EnableplaceholderJquery::Rails do
   context 'VERSION constant' do
     it 'is a valid SemVer string (semver.org)' do
       version = EnableplaceholderJquery::Rails::VERSION
-      expect { Semantic::Version.new version }.to_not raise_error()
+      expect { Semantic::Version.new version }.to_not raise_error
     end # it
   end # context
 end # describe
